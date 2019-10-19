412b68e7f2ba4aa0f70d18e41948167d66e94667
Ólafur Páll Geirsson <olafurpg@gmail.com>
2019-10-19 13:13:22 +0100

    Further changes

--- bot.js
+++ bot.js
@@ -1,5 +1,9 @@
 var Discord = require("discord.js");
-var auth = require("./auth.json");
+
+const token = process.env["DISCORD_TOKEN"];
+if (!token) {
+  throw new Error("missing environment variable: DISCORD_TOKEN");
+}
 
 // Configure logger settings
 const winston = require("winston");
@@ -8,33 +12,34 @@ const logger = winston.createLogger({
   format: winston.format.json(),
   transports: [
     new winston.transports.File({ filename: "error.log", level: "error" }),
-    new winston.transports.File({ filename: "combined.log" })
+    new winston.transports.File({ filename: "combined.log" }),
+    new winston.transports.Console({ format: winston.format.simple() })
   ]
 });
-if (process.env.NODE_ENV !== "production") {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple()
-    })
-  );
-}
 
-// Initialize Discord Bot
 var client = new Discord.Client();
 client.on("ready", () => {
   logger.info(`Logged in as ${client.user.tag}!`);
 });
 
+const isWarnedAboutMissingProfilePicture = new Set();
+
 client.on("message", msg => {
-  logger.info(msg.author.avatar);
-  logger.info(msg.author.avatarURL);
   switch (msg.channel.name) {
     case "experiment":
       if (msg.content == "ping") {
         msg.reply("pong");
       }
+
+      if (
+        !msg.author.avatarURL &&
+        !isWarnedAboutMissingProfilePicture.has(msg.author.id)
+      ) {
+        isWarnedAboutMissingProfilePicture.add(msg.author.id);
+        msg.reply("Can you please upload a profile picture?");
+      }
     default:
   }
 });
 
-client.login(auth.token);
+client.login(token);
